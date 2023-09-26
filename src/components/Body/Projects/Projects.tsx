2f7ace5f14fa398938a1d540776b84319bf3a0a1
Matheus Alves <miqueiasalves400@gmail.com>
2023-09-25 21:16:12 -0300

    temp

--- src/components/Body/Projects/Projects.tsx
+++ src/components/Body/Projects/Projects.tsx
@@ -1,7 +1,18 @@
-import Card from  '../../Card/Card'
+import Card from "../../Card/Card";
+import { Box, Flex, Text } from "@chakra-ui/react";
 
 export const Projects = () => {
-  return(
-    <Card />
-  )
-} 
+  return (
+    <Box bg="grey.400">
+      <Text fontSize="7xl" fontWeight={600} textAlign="center">
+        PROJECTS
+      </Text>
+      <Flex flexDirection='column' >
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </Flex>
+    </Box>
+  );
+};
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,14 +1,34 @@
-import { Heading, Box } from "@chakra-ui/react";
+import { Heading, Box, Image, Flex } from "@chakra-ui/react";
+import esfera from "../../assets/esfera.svg";
 
 export const Header = () => {
+  const sizeImg = { w: 24, h: 24 };
   return (
-    <Box bg='gray.600'>
-      <Heading as="h1" fontSize="12em" noOfLines={1}>
-        FRONTEND
-      </Heading>
-      <Heading as="h1" fontSize="12em" noOfLines={1}>
-        DEVELOPER
-      </Heading>
+    <Box bg="gray.400">
+      <Box ml='10%'>
+      <Flex alignItems="center" gap={6}>
+        <Heading as="h1" fontSize="12em" textAlign="start" noOfLines={1}>
+          FRONTEND
+        </Heading>
+        <Image src={esfera} alt="esfera" {...sizeImg} />
+        <Image src={esfera} alt="esfera" {...sizeImg} />
+        <Image src={esfera} alt="esfera" {...sizeImg} />
+      </Flex>
+      <Flex alignItems="center" gap={6}>
+        <Image src={esfera} alt="esfera" {...sizeImg} />
+        <Image src={esfera} alt="esfera" {...sizeImg} />
+        <Image src={esfera} alt="esfera" {...sizeImg} />
+        <Heading
+          as="h1"
+          fontSize="12em"
+          mt={"-2rem"}
+          textAlign="center"
+          noOfLines={1}
+        >
+          DEVELOPER
+        </Heading>
+      </Flex>
+      </Box>
     </Box>
   );
 };
